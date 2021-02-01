8982960faf8fd49899662daf0a4cd8a378c831bd
douglasmatosdev <douglasmatosdev@gmail.com>
2021-01-31 21:17:34 -0300

    Update name file styles

--- src/views/Home.view.jsx
+++ src/views/Home.view.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import App from '../components/App/App'
+import App from '../components/App'
 
-function HomeView() {
-    return <div>
-        <App />
-    </div>
+function HomeView () {
+  return <div>
+    <App />
+  </div>
 }
 
 export default HomeView

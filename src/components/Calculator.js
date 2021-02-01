b68f9ade97137c96aad3efd51c5e88fbc1ceeeff
douglasmatosdev <douglasmatosdev@gmail.com>
2021-01-31 22:24:00 -0300

    First test redux without hook useSelector

---
+++ src/components/Calculator.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { connect } from 'react-redux'
+
+function Calculator({ result }) {
+    return <>
+
+        <input type='text' placeholder='a' />
+        <input type='text' placeholder='b' />
+
+        <button>somar</button>
+        <button>subtrair</button>
+
+        <div>{result}</div>
+
+    </>
+}
+
+function mapStateToProps(state, ownProps) {
+    return {
+        result: state.calculator
+    }
+}
+
+export default connect(mapStateToProps)(Calculator)
